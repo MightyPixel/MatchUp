a10a39a2cd1183304f45ceff771d190b956af27b
adria-stef <adria.stef@gmail.com>
2014-11-08 13:34:43 +0200

    add login and register

--- matchup-client/app/scripts/controllers/auth.js
+++ matchup-client/app/scripts/controllers/auth.js
@@ -9,6 +9,8 @@
  */
 angular.module('matchupApp')
 .controller('AuthCtrl', function ($scope, $location, auth) {
+	$scope.isCoolUserName = false;
+
     $scope.login = function() {
         console.log('Logging in with the username/pass:');
         console.log($scope.username);
@@ -17,4 +19,16 @@ angular.module('matchupApp')
 
         $location.url('/home');
     };
+	
+	$scope.checkInput = function() {
+		if ($scope.username == 'Ogi') {
+			$scope.isCoolUserName = true;
+		} else {
+			$scope.isCoolUserName = false;
+		}
+	};
+	
+	$scope.register = function(){
+	}
+	
 });
