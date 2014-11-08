ac84938f26d35b91501a0617e8447b053e2da2c4
adria-stef <adria.stef@gmail.com>
2014-11-08 21:49:15 +0200

    create game and sreate team

--- matchup-client/app/scripts/controllers/team.js
+++ matchup-client/app/scripts/controllers/team.js
@@ -12,12 +12,18 @@ angular.module('matchupApp')
     $scope.availablePlayers = ['Ivan Ivanov', 'Martin Petrov', 'Valeri Nikolov', 'Nikolai Stefanov'];
 	
 	$scope.selectedPlayers = [];
-	
 	$scope.selectPlayer = function(player){
 		if (!_.find($scope.selectedPlayers, function(plr) { plr == player; })) {
 			 $scope.selectedPlayers.push(player);
 		}
 	}
-
+	$scope.playDates = [{}];
+	$scope.addPlayDate = function(playTime){
+		$scope.playDates[$scope.playDates.length - 1] = playTime;
+		$scope.playDates.push({});
+		//console.log(playTime)
+//		var d=document.getElementById("time");
+//		d.innerHTML+="<div class=\"row\" id=\"time\"><select class=\"form-control\"><option>Monday</option><option>Tuesday</option><option>Wednesday</option><option>Thursday</option><option>Friday</option><option>Saturday</option><option>Sunday</option></select><timepicker class=\"time\" hour-step=\"1\" minute-step=\"1\" show-meridian=\"false\" ng-model=\"mytime\" ng-change=\"changed()\"></timepicker><button type=\"button\" class=\"btn btn-success\" onclick=\"addTime()\"><bold>+</bold></button></div>";
+	}
 	
-  });
+});
